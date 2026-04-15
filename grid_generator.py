64d197aa03de996d5d221e5c72b7233e02f07599
amoore28-del <amoore28@terpmail.umd.edu>
2026-04-14 22:10:49 -0400

    Added loop for fish placement

--- grid_generator.py
+++ grid_generator.py
@@ -5,9 +5,18 @@ def generate_hidden_grid(rows, columns, num_fish)
 
     for row_num in range(rows):
         row = []
-        for colum_num in range (colum):
+        for col_num in range (col):
             row.append("-")
         grid.append(row)
+
+
+    fish_count= 0
+    while fish_count < num_fish:
+
+        row= random.randit(0, rows -1)
+        col= random.radint (0, cols -1)
+
+
 
 
 
